151b4fdbcde6a5e5db67181f456c01d6d6f8146e
Odey Abdalrahman <odey.aroo@gmail.com>
2022-02-01 08:22:14 +0400

    update imojes data.js

--- src/core/data/emojes.js
+++ src/core/data/emojes.js
@@ -1,8 +1,8 @@
 const getEmojes = () => {
   const emojes = [
     { id: 11, t_name: "Like", img:"/assets/images/lol.png", status: true },
-    { id: 22, t_name: "good", img:"/assets/images/happy.png", status: true },
     { id: 33, t_name: "very_good",   img:"/assets/images/sad.png",  status: true },
+    { id: 22, t_name: "good", img:"/assets/images/happy.png", status: true },
     { id: 44, t_name: "pissed_me", img: "/assets/images/angry.png", status: false },
   ];
   return emojes;
