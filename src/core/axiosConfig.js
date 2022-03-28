39f3a752bd4c7709923d6bd5d5de3a17b2c9e289
Odey Abdalrahman <odey.aroo@gmail.com>
2022-03-28 09:18:20 +0400

    edit axiosConfig

--- src/core/axiosConfig.js
+++ src/core/axiosConfig.js
@@ -3,8 +3,8 @@ import langService from "./services/langService";
 
 const api = ({ ...options }) => {
   const baseUrl = "https://evaluationapi.dhaid.shj.ae/api/";
-  const baseUrlTest = "https://localhost:5001/api/";
-  const api = axios.create({ baseURL: baseUrl })
+  const baseUrlTest = "https://localhost:44379/api/";
+  const api = axios.create({ baseURL: baseUrlTest })
   api.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
   api.defaults.headers.common["Consumer"] = "254321889";
   api.defaults.headers.common["Lang"] = langService().findByCode(langService().currentLangVal()).id;
